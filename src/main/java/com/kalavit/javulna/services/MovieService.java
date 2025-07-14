11230b2aefec75a085617e2f4363a1c901550a9d
Unzenon <habibanaapiasrisky@gmail.com>
2025-07-14 12:00:39 +0700

    fix xxe and sql injection

--- src/main/java/com/kalavit/javulna/services/MovieService.java
+++ src/main/java/com/kalavit/javulna/services/MovieService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.kalavit.javulna.services;
 
 import com.kalavit.javulna.dto.MovieDto;
@@ -11,9 +6,12 @@ import com.kalavit.javulna.services.autodao.MovieAutoDao;
 import java.io.ByteArrayInputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.XMLConstants;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,47 +23,43 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-/**
- *
- * @author peti
- */
 @Service
 public class MovieService {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(MovieService.class);
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
-    @Autowired
-    MovieAutoDao movieAutoDao;
-    
-    public List<MovieDto> findMovie(String title, String description, String genre, String id) {
-        int conditions = 0;
-        StringBuilder sql = new StringBuilder("select description, title, genre, id from movie ");
-        if (StringUtils.hasText(title)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("title LIKE '%").append(title).append("%'");
 
+    @Autowired
+    private MovieAutoDao movieAutoDao;
+
+    public List<MovieDto> findMovie(String title, String description, String genre, String id) {
+        StringBuilder sql = new StringBuilder("SELECT description, title, genre, id FROM movie");
+        StringBuilder whereClause = new StringBuilder();
+        List<Object> params = new ArrayList<>();
+
+        if (StringUtils.hasText(title)) {
+            addWhereClause(whereClause, "title LIKE ?");
+            params.add("%" + title + "%");
         }
         if (StringUtils.hasText(description)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("description LIKE '%").append(description).append("%'");
+            addWhereClause(whereClause, "description LIKE ?");
+            params.add("%" + description + "%");
         }
         if (StringUtils.hasText(genre)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("genre LIKE '%").append(genre).append("%'");
+            addWhereClause(whereClause, "genre LIKE ?");
+            params.add("%" + genre + "%");
         }
         if (StringUtils.hasText(id)) {
-            appendCondition(sql, conditions);
-            conditions++;
-            sql.append("id = '").append(id).append("'");
+            addWhereClause(whereClause, "id = ?");
+            params.add(id);
         }
-        LOG.debug(sql.toString());
-        List<MovieDto> users = this.jdbcTemplate.query(sql.toString(), new RowMapper<MovieDto>() {
+
+        sql.append(whereClause);
+        LOG.debug("Executing SQL: {}", sql);
+
+        return this.jdbcTemplate.query(sql.toString(), params.toArray(), new RowMapper<MovieDto>() {
             @Override
             public MovieDto mapRow(ResultSet rs, int rowNum) throws SQLException {
                 MovieDto ret = new MovieDto();
@@ -76,44 +70,52 @@ public class MovieService {
                 return ret;
             }
         });
-
-        return users;
     }
 
-    private void appendCondition(StringBuilder sb, int conditions) {
-        if (conditions == 0) {
-            sb.append(" where ");
+    private void addWhereClause(StringBuilder sb, String condition) {
+        if (sb.length() == 0) {
+            sb.append(" WHERE ").append(condition);
         } else {
-            sb.append(" and ");
+            sb.append(" AND ").append(condition);
         }
     }
-    
-    public Movie saveMovieFromXml(String xml){
+
+    public Movie saveMovieFromXml(String xml) {
         try {
-            Movie m = new Movie();
-            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            // Protect against XXE
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            dbf.setNamespaceAware(true);
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(new ByteArrayInputStream(xml.getBytes("UTF-8")));
             Element root = doc.getDocumentElement();
+
+            Movie m = new Movie();
             m.setTitle(getText(root, "title"));
             m.setDescription(getText(root, "description"));
             m.setGenre(getText(root, "genre"));
+
             movieAutoDao.save(m);
             return m;
         } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        } 
+            LOG.error("Error while saving movie from XML", ex);
+            throw new RuntimeException("Invalid XML data", ex);
+        }
     }
 
     private String getText(Element el, String tagName) {
         NodeList nl = el.getElementsByTagName(tagName);
-        if(nl != null && nl.getLength() >0){
+        if (nl != null && nl.getLength() > 0) {
             NodeList children = nl.item(0).getChildNodes();
-            if(children != null && children.getLength() > 0){
+            if (children != null && children.getLength() > 0) {
                 return children.item(0).getTextContent();
             }
         }
-        LOG.debug("no text content of tag with name: {}", tagName);
+        LOG.debug("No text content for tag: {}", tagName);
         return null;
     }
-    
 }
